ace02a6c20a96b287a967b8166ed7a5b2f426f93
PEDRO HENRIQUE SANTOS BRASILEIRO <pedro.hsbrasileiro@senacsp.edu.br>
2024-03-06 18:18:52 -0300

    Exercicio de Fibonacci

--- TesteTarget/src/main/java/com/target/testetarget/TesteTarget.java
+++ TesteTarget/src/main/java/com/target/testetarget/TesteTarget.java
@@ -4,13 +4,37 @@
 
 package com.target.testetarget;
 
-/**
- *
- * @author pedro.hsbrasileiro
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TesteTarget {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insira o numero: ");
+        int numero = scanner.nextInt();
+        ArrayList<Integer> fibonacci = new ArrayList<Integer>();
+        fibonacci.add(0);
+        fibonacci.add(1);
+        if(numero == 0 || numero == 1){
+            System.out.println("O numero esta na sequencia de Fibonacci");
+            System.exit(0);
+        }
+
+        for(int i = 0; i < Integer.MAX_VALUE;i++){
+            int valor = fibonacci.get(i) + fibonacci.get(i+1);
+            fibonacci.add(valor);
+            if(fibonacci.get(i) == numero){
+                System.out.println("O numero esta na sequencia de Fibonacci");
+                System.exit(0);
+            }
+            if(fibonacci.get(i) > numero){
+                System.out.println("O numero não esta na sequencia de Fibonacci");
+                System.exit(0);
+            }
+            
+        }
+        
+        
     }
 }
